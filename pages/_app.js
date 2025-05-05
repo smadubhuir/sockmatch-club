0dc70ff591a8c6b339b48d9e46179d4d77b0d252
smadubhuir <93944029+smadubhuir@users.noreply.github.com>
2025-05-05 17:19:16 -0500

    supabasesession

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+"use client";
+
 import "@/styles/globals.css";
+import { SupabaseProvider } from "@/context/SupabaseContext";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <SupabaseProvider>
+      <Component {...pageProps} />
+    </SupabaseProvider>
+  );
 }
 
 export default MyApp;

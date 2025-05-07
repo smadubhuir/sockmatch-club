195ab9ed8fec960fae32722bce8a0109ffa260d1
smadubhuir <93944029+smadubhuir@users.noreply.github.com>
2025-05-07 12:30:40 -0500

    navbar update

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,13 @@
 
 import "@/styles/globals.css";
 import { SupabaseProvider } from "@/context/SupabaseContext";
+import Navbar from "@/components/Navbar"; // Importing the Navbar component
 
 function MyApp({ Component, pageProps }) {
   return (
     <SupabaseProvider>
+      {/* Global Navbar for all pages */}
+      <Navbar />
       <Component {...pageProps} />
     </SupabaseProvider>
   );
